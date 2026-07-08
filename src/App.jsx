b12e4c89a3c91a61a3060699d83300ff735c6e28
Piyushify <piyushsharma3042007@gmail.com>
2026-07-08 11:35:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <>
-      <h1>Hi</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default Header
