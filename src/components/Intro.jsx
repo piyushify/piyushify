fa10d04178d22227563dbb4a4d49da2cf1980b21
Piyushify <piyushsharma3042007@gmail.com>
2026-07-22 12:35:02 +0530

    little bit more

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
   let info = "A BCA student."
   return (
     <section className="h-dvh flex items-center flex-col justify-center p-6">
-      <img src={dp} className="w-30 h-30 self-start rounded-full inset-shadow-sm shadow-sm"/>
+      <img src={dp} className="w-32 h-32 self-start rounded-full inset-shadow-sm shadow-sm"/>
       <h1 className="text-3xl md:text-5xl lg:text-7xl w-screen p-4 px-6 font-semibold">{greet}</h1>
       <p className="text-3xl md:text-5xl lg:text-7xl">I'm <b>Piyush Sharma</b>. <br/> {info} </p>
     </section>
